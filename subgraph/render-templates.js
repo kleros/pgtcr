f54816a826e2a2b9e88196cbe6829802ce90b33d
Green <40367733+greenlucid@users.noreply.github.com>
2025-06-02 17:25:23 +0200

    wip: subgraph

---
+++ subgraph/render-templates.js
@@ -0,0 +1,38 @@
+const fs = require('fs-extra');
+const mustache = require('mustache');
+
+const chainNameToChainId = {
+  gnosis: 100,
+  xdai: 100, // For Goldsky: https://docs.goldsky.com/chains/supported-networks
+  mainnet: 1,
+  goerli: 5,
+  sepolia: 11155111,
+};
+
+async function main() {
+  const networkName = process.argv[2];
+  const chainId = chainNameToChainId[networkName];
+  const deployments = JSON.parse(fs.readFileSync('networks.json', 'utf8'));
+  const { address: pgtcrFactory, startBlock: pgtcrFactoryStartBlock } =
+    deployments['PermanentGTCRFactory'][chainId];
+  const templateData = {
+    network: networkName,
+  };
+  templateData['PermanentGTCRFactory'] = {
+    address: pgtcrFactory,
+    addressLowerCase: pgtcrFactory.toLowerCase(),
+    startBlock: pgtcrFactoryStartBlock,
+  };
+
+  for (const templatedFileDesc of [['subgraph', 'yaml']]) {
+    const template = fs
+      .readFileSync(`${templatedFileDesc[0]}.template.${templatedFileDesc[1]}`)
+      .toString();
+    fs.writeFileSync(
+      `${templatedFileDesc[0]}.${templatedFileDesc[1]}`,
+      mustache.render(template, templateData),
+    );
+  }
+}
+
+main();
